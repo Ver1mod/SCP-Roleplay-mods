4b822c608abb59da3d11e1ba45f4f30eff6f9ed0
Ver1mod <132265283+Ver1mod@users.noreply.github.com>
2023-08-18 22:36:15 +0900

    Create list_of_mods.lua

---
+++ list_of_mods.lua
@@ -0,0 +1,18 @@
+local list = {
+  --Moderators & Developers
+  "Homboor", 
+  "Rynhex", 
+  "yuji071", 
+  "xXDrqgon", 
+  "deaconwtx", 
+  "Red_intern", 
+  "TrueShadowSpear", 
+  "GoszuGamer", 
+  "luckyluke1281mia",
+  "AntePavelicPoglavnik",
+  "happysully07",
+  "perciless"
+  "Steven_XP23",
+  "Chaosys",
+  "hack_tested"
+}
